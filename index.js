2d51ab969fb0c6f762beadae8579ccdaac53daf1
Marc <marc.ferraro1@gmail.com>
2021-02-03 00:34:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// function updateHTML(){
+
+    document.querySelector('main').remove()
+
+    // const newHeader = document.querySelector('head')
+
+    let newHeader = document.createElement('h1')
+
+    newHeader.id = 'victory'
+    newHeader.innerHTML = "is the champion"
+
+    document.body.appendChild(h1)
+
+// }
+
+// updateHTML()
